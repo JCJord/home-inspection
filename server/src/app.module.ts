d4602a81f03174cdcfbae12cad4cfa4ff5dc7dce
Julio Jordão <juliojc.jord@gmail.com>
2026-04-09 20:48:53 -0300

    chore: bundle recent created modules into app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InspectorsModule } from './inspectors/inspectors.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    InspectorsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
