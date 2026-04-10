c59b5495b804e4f8765bb352bedc9575505bec25
Julio Jordão <juliojc.jord@gmail.com>
2026-04-10 17:17:37 -0300

    feat:  update server to not update user email

--- server/src/inspectors/dto/update-profile.dto.ts
+++ server/src/inspectors/dto/update-profile.dto.ts
@@ -1,9 +1,6 @@
 import { IsEmail, IsOptional, IsString } from 'class-validator';
 
 export class UpdateProfileDto {
-  @IsOptional()
-  @IsEmail()
-  email?: string;
 
   @IsOptional()
   @IsString()
