1fa4983381eba64050d7285ae9f6a26bd9d27226
Julio Jordão <juliojc.jord@gmail.com>
2026-04-10 18:39:46 -0300

    feat: add icons nav bar desktop

--- client/src/app/features/dashboard/components/navbar-desktop/navbar-desktop.ts
+++ client/src/app/features/dashboard/components/navbar-desktop/navbar-desktop.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, House, ClipboardList, UserCircle } from 'lucide-angular';
 
 @Component({
   selector: 'app-navbar-desktop',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './navbar-desktop.html',
   styleUrl: './navbar-desktop.scss',
 })
-export class NavbarDesktop {}
+export class NavbarDesktop {
+  readonly icons = { House, ClipboardList, UserCircle };
+}
