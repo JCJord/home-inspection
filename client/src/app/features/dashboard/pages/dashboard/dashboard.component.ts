cf957c8d932f7b9cc0d02498c816d85917d601d5
Julio Jordão <juliojc.jord@gmail.com>
2026-04-10 18:44:46 -0300

    feat: active style on active route on nav bar buttons

--- client/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ client/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../../../core/services/auth.service';
 import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
