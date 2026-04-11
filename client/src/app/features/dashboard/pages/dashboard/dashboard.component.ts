7f30dbb6595455752c02a850261f5336d1e0bc2d
Julio Jordão <juliojc.jord@gmail.com>
2026-04-11 15:13:29 -0300

    chore:  remove logout from dashboard

--- client/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
+++ client/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,22 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
-import { AuthService } from '../../../../core/services/auth.service';
-import { ButtonComponent } from '../../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-  authService = inject(AuthService);
   private router = inject(Router);
-
-  onLogout() {
-    this.authService.logout();
-    this.router.navigate(['/auth/login']);
-  }
 }
