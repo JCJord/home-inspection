a9c439689d7ed43b12f88e433dc9dafc2c8bd13f
Julio Jordão <juliojc.jord@gmail.com>
2026-05-24 18:47:33 -0300

    update env var format

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -9,14 +9,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get<string>('DB_HOST', 'localhost'),
-        port: configService.get<number>('DB_PORT', 5432),
-        username: configService.get<string>('DB_USERNAME', 'postgres'),
-        password: configService.get<string>('DB_PASSWORD', 'postgres'),
-        database: configService.get<string>('DB_NAME', 'postgres'),
+        url: configService.get<string>('DATABASE_URL'),
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         autoLoadEntities: true,
-        synchronize: false, // Note: Set to false in production
+        synchronize: false,
         migrations: [__dirname + '/../migrations/*{.ts,.js}'],
         migrationsRun: true,
       }),
